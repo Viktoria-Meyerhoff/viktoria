da0d263d155945330014b9f38950befa4cf983d6
jeffrey_haemer <jeffrey.haemer@gmail.com>
2022-03-18 15:33:04 +0000

    Make a function to print headers.

--- dashboard.sh
+++ dashboard.sh
@@ -1,22 +1,29 @@
 #!/bin/bash -eux
-echo "CPU AND MEMORY RESOURCES ----------------------------"
-echo "      "
+
+header() {
+	echo "$@" '----------------------------'
+	echo
+}
+
+header "CPU AND MEMORY RESOURCES"
+
 echo "CPU Load Average:   "
 uptime | awk '{ for (i=8; i <= 10; ++i) printf $i" "; print""}'
 echo "Free RAM:"
 cat /proc/meminfo | sed -n '2p' | awk '{ for (i=2; i <=4; ++i) printf $i" "; print""}'
-echo "    "
-echo "NETWORK CONNECTIONS ----------------------------------"
+
+header "NETWORK CONNECTIONS"
+
 echo "lo Bytes Received:"
 ifconfig | sed -n 14p | awk '{print $5}'
 echo "lo Bytes Transmitted:"
 ifconfig | sed -n 16p | awk '{print $5}'
-echo "   "
+echo
 echo "enp0s3 Bytes Received:"
 ifconfig | sed -n 5p | awk '{print $5}'
 echo "enp0s3 Bytes Transmitted:"
 ifconfig | sed -n 7p | awk '{print $5}'
-echo "   "
+echo
 echo "Internet Connectivity"
 if ping -c 1 www.google.com > /dev/null
 then
@@ -24,9 +31,10 @@ then
 else
         echo "no"
 fi
-echo "     "
-echo "ACCOUNT INFORMATION ----------------------------------"
-echo "    "
+echo
+
+header "ACCOUNT INFORMATION"
+
 echo "Total Users:"
 cat /etc/passwd | wc -l 
 echo "Number Active:"
@@ -39,10 +47,10 @@ cat /etc/passwd | grep /bin/bash | wc -l
 echo "/bin/false:"
 cat /etc/passwd | grep /bin/false | wc -l
 
-echo "Filesystem Information"
+header "FILESYSTEM iNFORMATION"
+
 echo "Total Number of Files"
 cd /
 echo */ | wc | awk '{print $3}'
 echo "Total Number of Directories"
 echo */ | wc | awk '{print $2}'
-
